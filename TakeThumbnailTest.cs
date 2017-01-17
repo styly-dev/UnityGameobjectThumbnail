b8a6bb2a900cd2e127809902bd053863995bb77c
Yamaguchi@GTune1060 <yamaguchi@psychic-vr-lab.com>
2017-01-17 20:09:27 +0900

    一通り動く

---
+++ TakeThumbnailTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeThumbnailTest : MonoBehaviour {
+
+    //サムネイルを作成するオブジェクト
+    public GameObject TargetObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        //サムネイル作成
+        GameObject unit = Instantiate(TargetObject, Vector3.zero, Quaternion.identity) as GameObject;
+        UnityGameobjectThumbnail.makeThumbnailPng_from_Gameobject(unit, "c:/temp/temp1.png", 2300, 2300);
+    }
+
+}
